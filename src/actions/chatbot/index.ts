165a507838855c79de720f231036174c0fe06e4c
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-09-04 20:45:31 +0530

    chat-bot > SA: onAiChatBotAssistant init

--- src/actions/chatbot/index.ts
+++ src/actions/chatbot/index.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import client from "@/lib/prisma"
+import { extractEmailsFromString } from "@/lib/utils"
 
 export const onGetCurrentChatBot = async (id: string) => {
     try {
@@ -32,3 +33,117 @@ export const onGetCurrentChatBot = async (id: string) => {
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+let customerEmail: string | undefined
+
+export const onAiChatBotAssistant = async (
+    id: string,
+    chat: { role: 'assistant' | 'user'; content: string }[],
+    author: 'user',
+    message: string
+) => {
+    try {
+        const chatBotDomain = await client.domain.findUnique({
+            where: {
+                id,
+            },
+            select: {
+                name: true,
+                filterQuestions: {
+                    where: {
+                        answered: null,
+                    },
+                    select: {
+                        question: true,
+                    },
+                },
+            },
+        })
+        if (chatBotDomain) {
+            const extractedEmail = extractEmailsFromString(message)
+            if (extractedEmail) {
+                customerEmail = extractedEmail[0]
+            }
+
+            if (customerEmail) {
+                const checkCustomer = await client.domain.findUnique({
+                    where: {
+                        id,
+                    },
+                    select: {
+                        User: {
+                            select: {
+                                clerkId: true,
+                            },
+                        },
+                        name: true,
+                        customer: {
+                            where: {
+                                email: {
+                                    startsWith: customerEmail,
+                                },
+                            },
+                            select: {
+                                id: true,
+                                email: true,
+                                questions: true,
+                                chatRoom: {
+                                    select: {
+                                        id: true,
+                                        live: true,
+                                        mailed: true,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                })
+                if (checkCustomer && !checkCustomer.customer.length) {
+                    const newCustomer = await client.domain.update({
+                        where: {
+                            id,
+                        },
+                        data: {
+                            customer: {
+                                create: {
+                                    email: customerEmail,
+                                    questions: {
+                                        create: chatBotDomain.filterQuestions,
+                                    },
+                                    chatRoom: {
+                                        create: {},
+                                    },
+                                },
+                            },
+                        },
+                    })
+                    if (newCustomer) {
+                        console.log('new customer made')
+                        const response = {
+                            role: 'assistant',
+                            content: `Welcome aboard ${customerEmail.split('@')[0]
+                                }! I'm glad to connect with you. Is there anything you need help with?`,
+                        }
+                        return { response }
+                    }
+                }
+                if (checkCustomer && checkCustomer.customer[0].chatRoom[0].live) {
+                    // SA: onStoreConversations
+                }
+
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
