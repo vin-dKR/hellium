51b2b1f3b0da804aaeb34d67ddee68447e90476b
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-09-24 23:29:25 +0530

    dash > api > stripe > connect-acc

--- src/app/api/stripe/connect/connect.ts
+++ src/app/api/stripe/connect/connect.ts
@@ -11,6 +11,26 @@ export async function GET() {
     try {
         const user = await currentUser()
         if (!user) return new NextResponse('User not authenticated')
+
+        const account = await stripe.accounts.create({
+            country: 'US',
+            type: 'custom',
+            business_type: 'company',
+            capabilities: {
+                card_payments: {
+                    requested: true,
+                },
+                transfers: {
+                    requested: true,
+                },
+            },
+            external_account: 'btok_us',
+            tos_acceptance: {
+                date: 1547923073,
+                ip: '172.18.80.19',
+            },
+        })
+        
     } catch (error) {
         console.log(error)
     }
