7e60ee00de6e2c6932e286189da5f90e99c006ca
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-09-28 22:19:43 +0530

    dash > comp > product > ProductTable init

--- src/components/products/ProductTable.tsx
+++ src/components/products/ProductTable.tsx
@@ -3,7 +3,15 @@ import React from 'react'
 
 const ProductTable = (props: ProductTableProps) => {
     return (
-        <div>ProductTable</div>
+        <div>
+            <div>
+                <h2 className="font-bold text-2xl">Products</h2>
+                <p className="text-sm font-light">
+                    Add products to your store and set them live to accept payments from
+                    customers.
+                </p>
+            </div>
+        </div>
     )
 }
 
