6e23caadd24f68e6bc3d101d2a9f1644776bc9eb
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-10-05 22:39:13 +0530

    portal > payment > custId :questions implemented

--- src/app/portal/[domianid]/payment/[customerid]/index.tsx
+++ src/app/portal/[domianid]/payment/[customerid]/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import { onDomainCustomerResponses, onGetAllDomainBookings } from '@/actions/appointment'
+import PortalForm from '@/components/forms/portal/PortalForm'
 
 const CustomerPaymentPage = async ({ params }: {
     params: { domainId: string, customerId: string }
 }) => {
+    const questions = await onDomainCustomerResponses(params.customerId)
+
+    if (!questions) return null
+
     return (
-        <div>index</div>
+        <PortalForm
+            email={questions.email!}
+        // WIP for products
+        />
     )
 }
 
