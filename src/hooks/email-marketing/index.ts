66787599bc17e720e6da4e7a34b55c293c1121d9
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-10-17 00:18:08 +0530

    schemas : marketing-schema ✔️

--- src/hooks/email-marketing/index.ts
+++ src/hooks/email-marketing/index.ts
@@ -1,4 +1,20 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
 
 export const useEmailMarketing = () => {
-    
+    const [isSelected, setIsSelected] = useState<string[]>([])
+    const [loading, setLoading] = useState<boolean>(false)
+    const [campaignId, setCampaignId] = useState<string | undefined>()
+    const [processing, setProcessing] = useState<boolean>(false)
+    const [isId, setIsId] = useState<string | undefined>()
+    const [editing, setEditing] = useState<boolean>(false)
+
+    const {register,
+        handleSubmit,
+        formState: {errors},
+        reset
+    } = useForm({
+        resolver: zodResolver(EmailMarketingSchema)
+    })
 }
