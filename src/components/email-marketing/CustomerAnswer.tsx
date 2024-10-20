f4b811db8b7b0ca455b8a1c2bb6aa291f0af1170
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-10-20 16:09:04 +0530

    comp > email-mark > CustomerAnswer✔️

--- src/components/email-marketing/CustomerAnswer.tsx
+++ src/components/email-marketing/CustomerAnswer.tsx
@@ -1,9 +1,26 @@
+import { useAnswers } from '@/hooks/email-marketing/useEmailMarketing'
 import React from 'react'
+import { Loader } from '@/components/loader/Loader'
+import { CardDescription } from '../ui/card'
 
 const CustomerAnswer = ({ id }: IdProps) => {
+    const { answers, loading } = useAnswers(id)
+
     return (
-        <div>
-            
+        <div className="flex flex-col gap-5 mt-10">
+            <Loader loading={loading}>
+                {answers.map((answer) =>
+                    answer.customer.map((customer) =>
+                        customer.questions.length > 0 &&
+                        customer.questions.map((question, key) => (
+                            <div key={key}>
+                                <p>{question.question}</p>
+                                <CardDescription>{question.answered}</CardDescription>
+                            </div>
+                        ))
+                    )
+                )}
+            </Loader>
         </div>
     )
 }
