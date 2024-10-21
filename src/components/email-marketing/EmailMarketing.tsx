c03c58c11cdb4cdc6b136214b3819e47bb6e726b
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-10-21 21:07:53 +0530

    comp > email-mar > EmailMArketing: Create Campaign & credits ✔️

--- src/components/email-marketing/EmailMarketing.tsx
+++ src/components/email-marketing/EmailMarketing.tsx
@@ -4,6 +4,10 @@ import React from 'react'
 import CustomerTable from './CustomerTable'
 import { Button } from '../ui/button'
 import { Plus } from 'lucide-react'
+import Modal from '../modal/Modal'
+import { Card, CardDescription } from '../ui/card'
+import { Loader } from '@/components/loader/Loader'
+import FormGenerator from '../forms/form-generator/FormGenerator'
 
 
 const EmailMarketing = ({ domains, campaign, subscription }: EmailMarketingProps) => {
@@ -44,6 +48,43 @@ const EmailMarketing = ({ domains, campaign, subscription }: EmailMarketingProps
                     >
                         <Plus /> Add to Campaign
                     </Button>
+                    <Modal
+                        title="Create a new campaign"
+                        description="Add your customers and create a marketing campaign"
+                        trigger={
+                            <Card className="flex gap-2 items-center px-3 cursor-pointer text-sm">
+                                <Loader loading={false}>
+                                    <Plus />
+                                </Loader>
+                            </Card>
+                        }
+                    >
+                        <form
+                            className="flex flex-col gap-4"
+                            onSubmit={onCreateCampaign}
+                        >
+                            <FormGenerator
+                                name="name"
+                                register={register}
+                                errors={errors}
+                                inputType="input"
+                                placeholder="your campaign name"
+                                type="text"
+                            />
+                            <Button
+                                className="w-full"
+                                disabled={loading}
+                                type="submit"
+                            >
+                                <Loader loading={loading}>Create Campaign</Loader>
+                            </Button>
+                        </form>
+                    </Modal>
+                    <Card className="p-2">
+                        <CardDescription className="font-bold">
+                            {subscription?.credits} credits
+                        </CardDescription>
+                    </Card>
                 </div>
             </div>
         </div>
