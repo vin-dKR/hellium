faf8b39aa95a34089ba57ad27eba35b17e20e5ea
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-10-26 16:15:51 +0530

    Billing settings SubscriptionForm init

--- src/components/settings/BillingSettings.tsx
+++ src/components/settings/BillingSettings.tsx
@@ -4,7 +4,7 @@ import SectionLabel from '../section-label/SectionLabel'
 import { Card, CardContent, CardDescription } from '../ui/card'
 import { CheckCircle2, Plus } from 'lucide-react'
 import { pricingCards } from '@/constants/pricing'
-import { features } from 'process'
+import Modal from '../modal/Modal'
 
 const BillingSettings = async () => {
 
@@ -23,16 +23,23 @@ const BillingSettings = async () => {
           message="Add payment information, upgrade and modify your plan." />
       </div>
       <div className="lg:col-span-2 flex justify-start lg:justify-center ">
-        <Card className="border-dashed bg-cream border-gray-400 w-full cursor-pointer h-[270px] flex justify-center items-center">
-          <CardContent className="flex gap-2 items-center">
-            <div className="rounded-full border-2 p-1">
-              <Plus className="text-gray-400" />
-            </div>
-            <CardDescription className="font-semibold">
-              Upgrade Plan
-            </CardDescription>
-          </CardContent>
-        </Card>
+      	<Modal 
+	title="Choose a plan."
+	description="Tell us about yourself! What you do? Let’s tailor your experience so it best suits you."
+	trigger={
+		<Card className="border-dashed bg-cream border-gray-400 w-full cursor-pointer h-[270px] flex justify-center items-center">
+          		<CardContent className="flex gap-2 items-center">
+            			<div className="rounded-full border-2 p-1">
+              				<Plus className="text-gray-400" />
+            			</div>
+            			<CardDescription className="font-semibold">
+              				Upgrade Plan
+            			</CardDescription>
+          		</CardContent>
+        	</Card>
+	}>
+		// subscription form tsx
+	</Modal>
       </div>
       <div className="lg:col-span-2">
         <h3 className="text-lg mb-2 font-semibold">Current Paln</h3>
