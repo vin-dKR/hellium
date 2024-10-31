f213840d3f5ea04cd84991cf976546b07fefab7b
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-10-31 21:31:59 +0530

    SA > dashboard: getUserBalance ✔️✔️

--- src/actions/dashboard/index.ts
+++ src/actions/dashboard/index.ts
@@ -1,25 +1,63 @@
-"use server"
-import { currentUser } from "@clerk/nextjs"
-import client from "@/lib/prisma"
+'use server';
+import { currentUser } from '@clerk/nextjs';
+import client from '@/lib/prisma';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET!, {
+  typescript: true,
+  apiVersion: '2024-06-20',
+});
 
 export const getUserClients = async () => {
-	try {
-		const user = await currentUser()
-		if (user) {
-			const clients = await client.customer.count({
-				where: {
-					Domain: {
-						User: {
-							clerkId: user.id
-						}
-					}
-				},	
-			})
-			if (clients) {
-				return clients
-			}
-		}
-	} catch(e) {
-		console.log(e)
-	}
-}
+  try {
+    const user = await currentUser();
+    if (user) {
+      const clients = await client.customer.count({
+        where: {
+          Domain: {
+            User: {
+              clerkId: user.id,
+            },
+          },
+        },
+      });
+      if (clients) {
+        return clients;
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getUserBalance = async () => {
+  try {
+    const user = await currentUser();
+
+    if (user) {
+      const connnectedStripe = await client.user.findUnique({
+        where: {
+          clerkId: user.id,
+        },
+        select: {
+          stripeId: true,
+        },
+      });
+      if (connnectedStripe && connnectedStripe.stripeId) {
+        const transcations = await stripe.balance.retrieve({
+          stripeAccount: connnectedStripe.stripeId!,
+        });
+
+        if (transcations) {
+          const sales = transcations.pending.reduce((total, next) => {
+            return total + next.amount;
+          }, 0);
+
+          return sales / 100;
+        }
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,13 +1,15 @@
+import { getUserClients } from '@/actions/dashboard';
 import DashboardCard from '@/components/dashboard/Dashboard';
 import InfoBar from '@/components/infobar/InfoBar';
 
-const Dashboard = () => {
+const Dashboard = async () => {
+  const clients = await getUserClients();
   return (
     <div>
       <InfoBar />
       <div className='overflow-y-auto w-full chat-window flex-1 h-1'>
         <div className='flex gap-5 flex-wrap'>
-          <DashboardCard />
+          <DashboardCard value={clients || 0} />
         </div>
       </div>
     </div>
