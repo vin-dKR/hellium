5b0fd29c47a31fbc27de1d8462cf36d3eec69921
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-10-31 23:56:35 +0530

    SA > appointment: getUserAppointment ✔️

--- src/actions/appointment/index.ts
+++ src/actions/appointment/index.ts
@@ -1,122 +1,146 @@
-'use server'
+'use server';
 
-import client from "@/lib/prisma"
+import client from '@/lib/prisma';
 
 export const onDomainCustomerResponses = async (customerId: string) => {
-    try {
-        const customerQestion = await client.customer.findUnique({
-            where: {
-                id: customerId
-            },
-            select: {
-                email: true,
-                questions: {
-                    select: {
-                        id: true,
-                        question: true,
-                        answered: true,
-                    }
-                }
-            }
-        })
+  try {
+    const customerQestion = await client.customer.findUnique({
+      where: {
+        id: customerId,
+      },
+      select: {
+        email: true,
+        questions: {
+          select: {
+            id: true,
+            question: true,
+            answered: true,
+          },
+        },
+      },
+    });
 
-        if (customerQestion) {
-            return customerQestion
-        }
-    } catch (error) {
-        console.log(error)
+    if (customerQestion) {
+      return customerQestion;
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const onGetAllDomainBookings = async (domainId: string) => {
-    try {
-        const bookings = await client.bookings.findMany({
-            where: {
-                id: domainId
-            },
-            select: {
-                slot: true,
-                date: true,
-            }
-        })
+  try {
+    const bookings = await client.bookings.findMany({
+      where: {
+        id: domainId,
+      },
+      select: {
+        slot: true,
+        date: true,
+      },
+    });
 
-        if (bookings) {
-            return bookings
-        }
-    } catch (error) {
-        console.log(error)
+    if (bookings) {
+      return bookings;
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const onBookNewAppointment = async (
-    domainId: string,
-    customerId: string,
-    slot: string,
-    date: string,
-    email: string
+  domainId: string,
+  customerId: string,
+  slot: string,
+  date: string,
+  email: string,
 ) => {
-    try {
-        const bookings = await client.customer.update({
-            where: {
-                id: customerId,
-            },
-            data: {
-                booking: {
-                    create: {
-                        domainId,
-                        slot,
-                        date,
-                        email
-                    }
-                }
-            }
-        })
-        if (bookings) {
-            return { status: 200, message: 'Booking created' }
-        }
-    } catch (error) {
-        console.log(error)
+  try {
+    const bookings = await client.customer.update({
+      where: {
+        id: customerId,
+      },
+      data: {
+        booking: {
+          create: {
+            domainId,
+            slot,
+            date,
+            email,
+          },
+        },
+      },
+    });
+    if (bookings) {
+      return { status: 200, message: 'Booking created' };
     }
-
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const onGetAllDomainBookingsForCurrentUser = async (clerkId: string) => {
-    try {
-        const bookings = await client.bookings.findMany({
-            where: {
-                Customer: {
-                    Domain: {
-                        User: {
-                            clerkId
-                        }
-                    }
-                }
+  try {
+    const bookings = await client.bookings.findMany({
+      where: {
+        Customer: {
+          Domain: {
+            User: {
+              clerkId,
             },
-            select: {
-                id: true,
-                slot: true,
-                createdAt: true,
-                date: true,
-                email: true,
-                domainId: true,
-                Customer: {
-                    select: {
-                        Domain: {
-                            select: {
-                                name: true,
-                            }
-                        }
-                    }
-                }
-            }
-        })
+          },
+        },
+      },
+      select: {
+        id: true,
+        slot: true,
+        createdAt: true,
+        date: true,
+        email: true,
+        domainId: true,
+        Customer: {
+          select: {
+            Domain: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
 
-        if (bookings) {
-            return {
-                bookings
-            }
-        }
-    } catch (error) {
-        console.log(error)
+    if (bookings) {
+      return {
+        bookings,
+      };
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getUserAppointments = async () => {
+  try {
+    const user = await currentUser();
+    if (user) {
+      const bookings = await client.bookings.count({
+        where: {
+          Customer: {
+            Domain: {
+              User: {
+                clerkId: user.id,
+              },
+            },
+          },
+        },
+      });
+
+      if (bookings) {
+        return bookings;
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
