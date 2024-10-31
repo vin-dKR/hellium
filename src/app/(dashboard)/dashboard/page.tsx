4658d4354c8f7c9c51bffef4fb9df26ebc0c5e63
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-10-31 20:21:39 +0530

    comp > dash: Dashboard Card init

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,17 @@
+import DashboardCard from '@/components/dashboard/Dashboard';
+import InfoBar from '@/components/infobar/InfoBar';
+
 const Dashboard = () => {
-	return(
-		<div>
-			Dashboard
-		</div>
-	)
-}
+  return (
+    <div>
+      <InfoBar />
+      <div className='overflow-y-auto w-full chat-window flex-1 h-1'>
+        <div className='flex gap-5 flex-wrap'>
+          <DashboardCard />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
