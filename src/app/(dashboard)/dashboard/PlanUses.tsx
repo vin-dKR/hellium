c6cb4ae08b381fcc7c683b582619ab95177b26f1
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-11-01 20:13:38 +0530

    dash > PlanUses: init

---
+++ src/app/(dashboard)/dashboard/PlanUses.tsx
@@ -0,0 +1,11 @@
+type PlanUses = {
+  plan: string;
+  credits: number;
+  domains: string[];
+  clients: number;
+};
+const PlanUses = ({ plan, credits, domains, clients }: PlanUses) => {
+  return <></>;
+};
+
+export default PlanUses;
