c63becdbdb44fae38ed1992ec4a4b2924420e20f
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-11-14 22:41:55 +0530

    new --

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,18 @@
-import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "@/context/themeProvider";
+import type { Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Toaster } from '@/components/ui/toaster';
+import { ThemeProvider } from '@/context/themeProvider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Hellium",
-  description: "AI sales bot.",
+  title: 'Hellium',
+  description: 'AI sales bot.',
+  icons: {
+    icon: '/images/Logo.svg',
+  },
 };
 
 export default function RootLayout({
@@ -19,11 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang='en'>
         <body className={inter.className}>
           <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
+            attribute='class'
+            defaultTheme='system'
             enableSystem
             disableTransitionOnChange
           >
